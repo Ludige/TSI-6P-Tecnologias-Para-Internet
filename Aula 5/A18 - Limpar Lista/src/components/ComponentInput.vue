a3f3b6ce0814a76b973ce4850d74ddb4f2862fc0
IFGoiano <thallythamonnycky@gmail.com>
2022-11-23 21:10:25 -0300

    Atv 18

---
+++ Aula 5/A18 - Limpar Lista/src/components/ComponentInput.vue
@@ -0,0 +1,51 @@
+<template>
+    <div>
+      <input v-model="campo">
+      <button @click="add" id="btn">+</button>
+
+      <ol>
+        Livros:
+        <li v-for="item in lista" :key="item">
+          {{ item }}
+        </li>
+      </ol>
+    </div>
+</template>
+
+<script>
+import { defineComponent } from 'vue';
+export default defineComponent({
+    data() {
+        return{
+          lista:[],
+          campo: "",
+        }
+    },
+    methods:{
+      add(){
+        if(this.campo != ""){
+          this.lista.push(this.campo);
+          this.campo = "";
+        }
+      }
+    }
+})
+</script>
+
+<style scoped>
+  input{
+    background-color: white;
+    border: 1.5px solid;
+    height: 35px;
+    width: 60vh;
+    padding-left: 10px;
+    border-radius: 10px;
+  }
+  #btn{
+    width: 3vw;
+    height: 5vh;
+    background-color: greenyellow;
+    margin-left: 10px;
+    border-radius: 50px;
+  }
+</style>
